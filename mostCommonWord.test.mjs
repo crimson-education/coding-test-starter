81dfbc6f69a29beb2c08afdbaeb3e6985d060c06
Wasiq Kashkari <w.kashkari@outlook.com>
2026-03-11 11:52:32 +1300

    Add useful discription

--- mostCommonWord.test.mjs
+++ mostCommonWord.test.mjs
@@ -1,7 +1,7 @@
 import { getMostCommonWord } from "./mostCommonWord.mjs";
 
 describe("testing mostCommonWord", () => {
-  it("should do something useful", () => {
+  it("should find the most common word in a basic string", () => {
     expect(
       getMostCommonWord("the quick brown fox jumps over the lazy dog"),
     ).toEqual("the");
